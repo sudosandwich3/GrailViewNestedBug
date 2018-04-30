7d98bbf1756a321078c86db63b5a03886e51d8d3
sudosandwich3 <itsduffy@gmail.com>
2018-04-30 13:03:00 -0400

    Updated val

--- grails-app/controllers/viewsnested/NestedTestController.groovy
+++ grails-app/controllers/viewsnested/NestedTestController.groovy
@@ -8,17 +8,17 @@ class NestedTestController {
 	static responseFormats = ['json', 'xml']
 	
     def test1() {
-      MyObj obj = new MyObj(val1: 'myVa2', val2: 'myVal2', val3: 'myVal3')
+      MyObj obj = new MyObj(val1: 'myVa1', val2: 'myVal2', val3: 'myVal3')
       respond obj
     }
 
     def test2() {
-      MyObj obj = new MyObj(val1: 'myVa2', val2: 'myVal2', val3: 'myVal3')
+      MyObj obj = new MyObj(val1: 'myVa1', val2: 'myVal2', val3: 'myVal3')
       respond obj
     }
 
     def test3() {
-      MyObj obj = new MyObj(val1: 'myVa2', val2: 'myVal2', val3: 'myVal3')
+      MyObj obj = new MyObj(val1: 'myVa1', val2: 'myVal2', val3: 'myVal3')
       respond obj
     }
 }
